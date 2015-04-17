8ef93d5ff3b44a4e9b4b2b343f4bb83888cb8218
qinjilei <qinjilei@qinjileideMacBook-Pro.local>
2015-04-17 10:19:26 +0800

    Initial Commit

---
+++ AppleChat/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AppleChat
+//
+//  Created by qinjilei on 15/4/17.
+//  Copyright (c) 2015年 qinjilei. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ AppleChat/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AppleChat
+//
+//  Created by qinjilei on 15/4/17.
+//  Copyright (c) 2015年 qinjilei. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
